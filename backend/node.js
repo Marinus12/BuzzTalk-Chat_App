72794ac538da5fba626a93907b7f929386a0b4a7
Evodiatembi <141280564+Evodiatembi@users.noreply.github.com>
2024-09-20 01:47:47 +0100

    Update node.js

--- backend/node.js
+++ backend/node.js
@@ -1,17 +1,1 @@
-import { MongoClient } from 'mongodb';
 
-async function connectToMongoDB() {
-    const uri = 'mongodb+srv://akumchaevodia:meldiah75@cluster0.s5yi7.mongodb.net/';
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-    try {
-        await client.connect();
-        console.log('Connected to MongoDB');
-    } catch (err) {
-        console.error(err);
-    } finally {
-        await client.close();
-    }
-}
-
-connectToMongoDB().catch(console.error);
